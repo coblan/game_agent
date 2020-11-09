8900c627cae22343a2f7190222f396f09a187943
hyl <coblan@163.com>
2020-11-10 00:36:40 +0800

    dd

--- src/settings/stage.py
+++ src/settings/stage.py
@@ -15,6 +15,23 @@ DATABASES = {
         'OPTIONS': {'charset':'utf8mb4'},
 
       },
+     'game_sqlserver': {
+         'NAME': 'bnsm_gamedb_trunk_individual',#'Sports',
+         'ENGINE': 'sql_server.pyodbc',
+          'HOST':'120.79.245.189,10824',
+          'USER':  'sa',
+         'PASSWORD': 'libi@123', 
+         'OPTIONS': {
+              'driver': 'ODBC Driver 17 for SQL Server',
+             #'autocommit': True,
+             #'host_is_server': True,
+             #'unicode_results': True,
+             #'driver': 'FreeTDS',
+             #'extra_params': 'tds_version=8.0',
+             
+               },
+                
+        } ,      
 } 
 
 ALLOWED_HOSTS=['*']
