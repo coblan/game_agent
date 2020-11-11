a8a86a8cd685a557834eb276ab02ef0cbe7c7d71
hyl <coblan@163.com>
2020-11-11 22:42:17 +0800

    dd

--- src/agent/views.py
+++ src/agent/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,HttpResponse
-from .models import AgentUser
+from .models import AgentUser,GamePlayer
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -17,12 +17,16 @@ def postregister(request):
     account = request.POST.get('account')
     pwd = request.POST.get('pwd')
     code = request.POST.get('code')
-    agent = request.POST.get('regist_code')
+    agent = request.POST.get('agent')
+    agent_user = AgentUser.objects.get(regist_code=agent)
     dc = {}
-    if not code:
+    if  GamePlayer.objects.filter(acount=account).exists():
+        dc['error']='该账号已经存在' 
+    elif not code:
         dc['error']='请填写验证码' 
     elif not pwd:
         dc['error']='必须填写密码' 
     else:
         dc ['success']= True
+    GamePlayer.objects.create(acount=account,agent=agent_user)
     return HttpResponse(json.dumps(dc), content_type="application/json")
