1659bf570d65041b66adff0a1b375961d658a0da
hyl <coblan@163.com>
2020-11-14 15:57:15 +0800

    dd

--- src/settings/prod.py
+++ src/settings/prod.py
@@ -9,7 +9,7 @@ DATABASES = {
         'NAME': 'game_agent',
         'ENGINE': 'django.db.backends.mysql',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root898767',
         'HOST': '127.0.0.1', 
         'PORT': '3306', 
         'OPTIONS': {'charset':'utf8mb4'},
