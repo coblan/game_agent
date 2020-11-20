bcb8f7354b51e8c502cc1a59e22f1db31775428d
hyl <coblan@163.com>
2020-11-20 13:59:39 +0800

    dd

--- src/agent/admin_home.py
+++ src/agent/admin_home.py
@@ -1,28 +1,39 @@
 from helpers.director.shortcut import page_dc
-from .models import GamePlayer,Recharge
+from .models import GamePlayer,Recharge,AgentRecharge
 from django.utils import timezone
 from django.db.models import Sum
 
 class MyHome(object):
     
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, request,*args, **kwargs):
+        self.crt_user = request.user
     
     def get_template(self):
         return 'jb_admin/live.html'
     
     def get_context(self):
         now = timezone.now()
-        player_count = GamePlayer.objects.filter(createtime__date=now.date()).count()
-        palyer_recharge_count = Recharge.objects.filter(createtime__date=now.date()).values('player').count()
-        total_amount = Recharge.objects.filter(createtime__date=now.date()).aggregate(total_amount=Sum('amount')).get('total_amount') or 0
+        if 1 in [x.pk for x  in self.crt_user.groups.all()]:
+            player_count = GamePlayer.objects.filter(agent=self.crt_user.agentuser,createtime__date=now.date()).count()
+            palyer_recharge_count = Recharge.objects.filter(createtime__date=now.date(),agent=self.crt_user.agentuser).values('player').count()
+            total_amount = Recharge.objects.filter(createtime__date=now.date(),agent=self.crt_user.agentuser).aggregate(total_amount=Sum('amount')).get('total_amount') or 0
         
-        rows =[
-            {'label':'新增用户','value':player_count} , 
-            {'label':'充值用户','value':palyer_recharge_count} ,   
-            {'label':'充值金额','value':total_amount} ,   
-            
-            ]
+            rows =[
+              {'label':'新增玩家','value':player_count} , 
+              {'label':'充值玩家数','value':palyer_recharge_count} ,   
+              {'label':'玩家充值金额','value':total_amount} ,   
+              ]
+        else:
+            player_count = GamePlayer.objects.filter(createtime__date=now.date()).count()
+            palyer_recharge_count = Recharge.objects.filter(createtime__date=now.date()).values('player').count()
+            total_amount = Recharge.objects.filter(createtime__date=now.date()).aggregate(total_amount=Sum('amount')).get('total_amount') or 0
+            agent_amount = AgentRecharge.objects.filter(createtime__date=now.date()).aggregate(total_amount=Sum('amount')).get('total_amount') or 0
+            rows =[
+                {'label':'新增玩家','value':player_count} , 
+                {'label':'充值玩家数','value':palyer_recharge_count} ,   
+                {'label':'玩家充值金额','value':total_amount} ,   
+                {'label':'代理充值金额','value':agent_amount},
+                ]
         #for row in rows:
             #if row.get('value') ==0:
                 #row.pop('action')
